56fc4148b5b3503e9452a5ec8ee53a981b7e229c
Mauricio Herrera Ortega <ronalli56@gmail.com>
2025-10-15 18:59:34 -0300

    init config

---
+++ app/pages/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div>
+  <div>hi</div>
+  <Button label="Verify" />
+  <UButton>Button</UButton>
+</div>
+</template>
+
+<style scoped>
+
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,48 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+import tailwindcss from "@tailwindcss/vite"
+import Aura        from "@primeuix/themes/aura"
+
+export default defineNuxtConfig( {
+  compatibilityDate: "2025-07-15",
+  devtools         : { enabled: true },
+  modules          : [
+    "@nuxt/ui",
+    "@nuxtjs/supabase",
+    "@prisma/nuxt",
+    "@pinia/nuxt",
+    "@vee-validate/nuxt",
+    "@primevue/nuxt-module",
+    "nuxt-security",
+    "@peterbud/nuxt-query"
+  ],
+  css              : ["~/assets/css/main.css", "primeicons/primeicons.css"],
+  vite             : {
+    plugins: [
+      tailwindcss()
+    ]
+  },
+  nuxtQuery        : {
+    autoImports: ["useQuery", "useInfiniteQuery", "useMutation"]
+  },
+
+  supabase:
+    {
+      redirect: false
+    },
+
+  primevue: {
+    options: {
+      theme: {
+        preset : Aura,
+        options:
+          {
+            darkModeSelector: "[theme=\"dark\"]"
+          }
+      }
+    }
+  }
+  ,
+
+  build: {
+    transpile: ["fp-ts"]
+  }
+} )
