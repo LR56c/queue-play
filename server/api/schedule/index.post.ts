efca0700730700157b868f10853138561a07e739
Mauricio Herrera Ortega <ronalli56@gmail.com>
2025-11-26 08:30:06 -0300

    add schedule CRUD operations

---
+++ server/api/schedule/index.post.ts
@@ -0,0 +1,34 @@
+import { isLeft }                  from "fp-ts/Either"
+import {
+  parseData
+}                                  from "~~/core/modules/shared/application/parse_handlers"
+import { schedulePlaylistService } from "~~/server/dependencies/dependencies"
+import {
+  schedulePlaylistResponse
+}                                  from "~~/core/modules/schedule_playlist/application/schedule_playlist_response"
+
+export default defineEventHandler( async ( event ) => {
+  const body = await readBody( event )
+
+  const dto = await parseData( schedulePlaylistResponse, body )
+
+  if ( isLeft( dto ) ) {
+    throw createError( {
+      statusCode   : 400,
+      statusMessage: "Bad Request"
+    } )
+  }
+
+  const result = await schedulePlaylistService.add( dto.right )
+  if ( isLeft( result ) ) {
+    throw createError( {
+      statusCode   : 400,
+      statusMessage: "Bad Request"
+    } )
+  }
+  // setHeader( event, "ut", await signJwt( result.right ) )
+  return {
+    statusMessage: "OK",
+    statusCode   : 200
+  }
+} )
